1dc97fea5b7ecdf3585c2f8ea597923aef346e13
miranda <miranda.devel@gmail.com>
2021-10-23 19:13:53 -0400

    update package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import './App.css';
-import HomePage from './pages/HomePage';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import AppRouter from "./routers/AppRouter";
 import AuthProvider from "./middlewares/AuthProvider";
 
